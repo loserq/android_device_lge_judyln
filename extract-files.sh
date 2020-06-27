89dd438175fab7473090e621e5b0c500a7999d68
loserq <2650009628@qq.com>
2020-06-27 16:40:56 +0800

    bringup:change vendor tool path

--- extract-files.sh
+++ extract-files.sh
@@ -25,9 +25,9 @@ VENDOR=lge
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+EXTHM_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$EXTHM_ROOT"/vendor/exthm/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
